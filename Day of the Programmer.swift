700e30a9fe3c7a0bbc7cb3a142529bc6c330ef8f
AhmetBaygin <37955475+AhmetBaygin@users.noreply.github.com>
2022-06-11 20:40:47 +0300

    Create Day of the Programmer.swift

---
+++ Day of the Programmer.swift
@@ -0,0 +1,26 @@
+import Foundation
+
+//simple solution no explanation
+func dayOfProgrammer(year: Int) -> String {
+    if year > 1918{
+        if (year % 4 == 0 && year % 25 != 0) || year % 400 == 0{ 
+            return "12.09.\(year)"
+        }
+        else {
+            return "13.09.\(year)"
+        }
+    }
+    else if year  == 1918{
+        return "26.09.\(year)"
+    }
+    else {
+        if year % 4 == 0{
+            return "12.09.\(year)"
+        }
+        else {
+            return "13.09.\(year)"
+        }
+    }
+    
+
+}
