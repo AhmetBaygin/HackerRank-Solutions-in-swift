a8bb6a01b3e6c18648f00d77a4011a30204e1151
AhmetBaygin <37955475+AhmetBaygin@users.noreply.github.com>
2022-06-14 14:11:51 +0300

    Create Cats and a Mouse.swift

---
+++ Cats and a Mouse.swift
@@ -0,0 +1,13 @@
+import Foundation
+
+func catAndMouse(x: Int, y: Int, z: Int) -> String {
+    if abs(x-z)<abs(y-z){
+        return "Cat A"
+    }
+    else if abs(x-z)>abs(y-z) {
+        return "Cat B"
+    }
+    else{
+        return "Mouse C"
+    }
+}
